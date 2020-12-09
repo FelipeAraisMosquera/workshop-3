78264d009fc733907b842837e812dce0646b9b33
Felipe <felipe.arias.6183@eam.edu.co>
2020-12-08 19:59:32 -0700

    pug and nodemon install

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express') // importamos framework el express
 const app = express() // creamos una variable que acceda al express
 const port = 3000 // Creamos un variable para definir el puerto
 
+app.set('view engine','pug');
+app.use(express.static(__dirname + '/public'));
+
 //nos traemos el de express el metodo get para retornar una respuesta
 // Cuando el cliente ingrese a la ruta '/'
 app.get('/', (req, res) => { // 
-  res.send('Hello World!')
+  res.send('Hello World......! ')
 })
 
 // nos traemnos de express el metodo listen para levantar el proyecto en le puerto definido
