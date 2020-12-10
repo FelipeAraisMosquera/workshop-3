f0161229659e3639170d5b6414b6c8682fff5588
Felipe <felipe.arias.6183@eam.edu.co>
2020-12-09 21:46:46 -0700

    template viewes, call database, styles base

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const express = require('express') // importamos framework el express
 const app = express() // creamos una variable que acceda al express
 const port = 3000 // Creamos un variable para definir el puerto
 
+const data = require('./database/data.json');
+
 app.set('view engine','pug');
 app.use(express.static(__dirname + '/public'));
 
 //nos traemos el de express el metodo get para retornar una respuesta
 // Cuando el cliente ingrese a la ruta '/'
 app.get('/', (req, res) => { // 
-  res.send('Hello World......! ')
-})
-
+  res.render('index',{
+    data: data.albums
+});
+});
 // nos traemnos de express el metodo listen para levantar el proyecto en le puerto definido
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
